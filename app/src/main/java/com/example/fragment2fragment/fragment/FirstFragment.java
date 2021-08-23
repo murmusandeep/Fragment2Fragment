e8a5b351ecc280bbcd43963aa47cfa1f001dd051
murmusandeep <murmusandeep17@gmail.com>
2021-08-23 11:00:06 +0530

    first commit

--- app/src/main/java/com/example/fragment2fragment/fragment/FirstFragment.java
+++ app/src/main/java/com/example/fragment2fragment/fragment/FirstFragment.java
@@ -2,11 +2,16 @@ package com.example.fragment2fragment.fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentResultListener;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.fragment2fragment.R;
 
@@ -17,6 +22,32 @@ public class FirstFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_first, container, false);
+
+        Button DataTOFragment2 = view.findViewById(R.id.sendDataToFragment2);
+
+        DataTOFragment2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                EditText dataTo2 = view.findViewById(R.id.dataToFragment2);
+                Bundle result = new Bundle();
+
+                result.putString("df1", dataTo2.getText().toString().trim());
+                getParentFragmentManager().setFragmentResult("dataFrom1", result);
+                dataTo2.setText("");
+            }
+        });
+
+        getParentFragmentManager().setFragmentResultListener("dataFrom2", this, new FragmentResultListener() {
+            @Override
+            public void onFragmentResult(@NonNull String requestKey, @NonNull Bundle result) {
+
+                String data = result.getString("df2");
+                TextView textView = view.findViewById(R.id.dataFrom1);
+                textView.setText(data);
+            }
+        });
 
         return view;
     }
--- app/src/main/java/com/example/fragment2fragment/fragment/SecondFragment.java
+++ app/src/main/java/com/example/fragment2fragment/fragment/SecondFragment.java
@@ -2,11 +2,16 @@ package com.example.fragment2fragment.fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentResultListener;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.fragment2fragment.R;
 
@@ -18,6 +23,32 @@ public class SecondFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view =  inflater.inflate(R.layout.fragment_second, container, false);
+
+        Button DataTOFragment1 = view.findViewById(R.id.sendDataToFragment1);
+
+        DataTOFragment1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                EditText dataTo1 = view.findViewById(R.id.dataToFragment1);
+                Bundle result = new Bundle();
+
+                result.putString("df2", dataTo1.getText().toString().trim());
+                getParentFragmentManager().setFragmentResult("dataFrom2", result);
+                dataTo1.setText("");
+            }
+        });
+
+        getParentFragmentManager().setFragmentResultListener("dataFrom1", this, new FragmentResultListener() {
+            @Override
+            public void onFragmentResult(@NonNull String requestKey, @NonNull Bundle result) {
+
+                String data = result.getString("df1");
+                TextView textView = view.findViewById(R.id.dataFrom1);
+                textView.setText(data);
+            }
+        });
 
         return view;
     }
